7e9cd34e154f0d6c79b916cfeac1a3beda08b2ab
Luong Van Dai <80164034+luongvdai@users.noreply.github.com>
2023-09-21 04:13:02 +0700

    submit
    submit

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,1 +1,16 @@
 // Your JS code goes here
+var btnAdd = document.getElementById("btnAdd");
+var Addmodal = document.getElementById("addModal");
+var Deletemodal = document.getElementById("deleteModal");
+btnAdd.addEventListener("click", function () {
+  openAdd();
+});
+window.onclick = function (event) {
+  if (event.target == Addmodal || event.target == Deletemodal) {
+    Addmodal.style.display = "none";
+    Deletemodal.style.display = "none";
+  }
+};
+function openAdd() {
+  Addmodal.style.display = "block";
+}
